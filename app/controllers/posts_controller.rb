c3bec9904bae530254e064929d0ffa133a408202
wifelette <LeahSilber@gmail.com>
2019-12-26 16:14:25 -0800

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,11 @@
-class PostsController < ApplicationController
-	def index
-		@posts = Post.all
-	end
+# frozen_string_literal: true
 
-	def show
-		@post = Post.find(params[:id])
-	end
+class PostsController < ApplicationController
+  def index
+    @posts = Post.all
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
 end
